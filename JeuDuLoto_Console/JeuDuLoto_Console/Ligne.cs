d38dcf688510e81e8831cf2d852ef32e30b0b6dc
Fabrice Foray <farbice.foray@lyceeastier.com>
2020-03-28 16:45:52 +0100

    Début de la classe Ligne

--- JeuDuLoto_Console/JeuDuLoto_Console/Ligne.cs
+++ JeuDuLoto_Console/JeuDuLoto_Console/Ligne.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace JeuDuLoto_Console
 {
-    class Ligne
+    public class Ligne
     {
         // 
         int[] lesNumeros;
@@ -17,16 +17,34 @@ namespace JeuDuLoto_Console
             lesNumeros = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         }
 
+        public bool EstVide
+        {
+            get
+            {
+                // Une ligne est vide si tous les numéros sont à zéro
+                bool vide = true;
+                foreach (int number in lesNumeros)
+                {
+                    if ( number != 0 )
+                    {
+                        vide = false;
+                        break;
+                    }
+                }
+                return vide;
+            }
+        }
+
         // Ici, on appelle le constructeur par défaut, avant d'executer le code
-        public Ligne( int[] initNumbers ):this()
+        public Ligne(int[] initNumbers) : this()
         {
             // Une ligne contient 5 nombres, si ce n'est pas bon ... -->> Exception !
-            if ( ( initNumbers != null ) && ( initNumbers.Length != 5 ) )
+            if ((initNumbers == null) || (initNumbers.Length != 5))
             {
                 throw new Exception("L'initialisation de la ligne est incorrecte: La taille est incorrecte");
             }
             // On va remplir et vérifier
-            foreach( int number in initNumbers )
+            foreach (int number in initNumbers)
             {
                 // Quelle dizaine ?
                 int dizaine = number / 10;
@@ -34,7 +52,7 @@ namespace JeuDuLoto_Console
                 if (number == 90)
                     dizaine = 8;
                 // La case déjà remplie !?
-                if ( lesNumeros[ dizaine ] != 0 )
+                if (lesNumeros[dizaine] != 0)
                 {
                     throw new Exception("L'initialisation de la ligne est incorrecte: Deux nombres pour la même dizaine.");
                 }
@@ -44,9 +62,24 @@ namespace JeuDuLoto_Console
                 }
             }
         }
-        Boolean EstQuine( int[] NombresAVerifier )
+
+        Boolean EstQuine(int[] NombresAVerifier)
         {
             return false;
+        }
+
+        Boolean EstPresent(int NombreAVerifier)
+        {
+            bool exist = false;
+            foreach (int number in lesNumeros)
+            {
+                if (number == NombreAVerifier)
+                {
+                    exist = true;
+                    break;
+                }
+            }
+            return exist;
         }
     }
 }
