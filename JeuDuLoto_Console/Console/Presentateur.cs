4b0758aa59127adf10bbd81c405d0daf3b58ae2b
Fabrice Foray <farbice.foray@lyceeastier.com>
2020-03-30 19:09:26 +0200

    Création du présentateur Console

--- JeuDuLoto_Console/Console/Presentateur.cs
+++ JeuDuLoto_Console/Console/Presentateur.cs
@@ -9,14 +9,58 @@ namespace JeuDuLoto_Console
 {
     class Presentateur
     {
-        public void Afficher( Carton leCarton )
+        public void Afficher(Carton leCarton)
         {
-            
+            // On a 9 Colonnes, chacune contient un nombre sur 2 caractères
+            // On va écrire chaque case sous la forme !XX
+            // Et on ajoutera ! à la fin de la ligne pour fermer
+            // On a donc besoin d'une ligne horizontale de 3 * 9 + 1 caractères
+            // Maintenant, on affiche le contenu
+            this.LigneHorizontale();
+            this.SautLigne();
+            Ligne laLigne;
+            for (int i = 0; i < 3; i++)
+            {
+                laLigne = leCarton[i];
+                for (int j = 0; j < 9; j++)
+                {
+                    this.SeparateurCase();
+                    this.Case(laLigne[j], laLigne[1,j] == 1 );
+                }
+                this.SeparateurCase();
+                this.SautLigne();
+            }
+            this.LigneHorizontale();
+            this.SautLigne();
         }
 
-        public void Tester(Carton leCarton)
+        private void LigneHorizontale()
         {
-            throw new System.NotImplementedException();
+            String barre = new string('-', 46);
+            Console.Write(barre);
+        }
+
+        private void Case(int nombre, bool marque )
+        {
+            Console.Write(" ");
+            if (nombre == 0)
+                Console.Write("  ");
+            else
+                if (marque)
+                Console.Write("XX");
+            else
+                Console.Write(String.Format("{0,2}", nombre));
+            Console.Write(" ");
+        }
+
+        private void SeparateurCase()
+        {
+            Console.Write("!");
+        }
+
+        private void SautLigne()
+        {
+            Console.WriteLine();
         }
     }
 }
--- JeuDuLoto_Console/Console/Program.cs
+++ JeuDuLoto_Console/Console/Program.cs
@@ -11,12 +11,25 @@ namespace JeuDuLoto_Console
     {
         static void Main(string[] args)
         {
+            // On crée le génerateur
             GenerateurDeCarton fabrique = new GenerateurDeCarton();
-
+            // et on récupère un carton
             Carton unCarton = fabrique.Generer();
-
+            // Maintenant, on affiche ce Carton avec notre présentateur
             Presentateur pres = new Presentateur();
             pres.Afficher(unCarton);
+            //
+            int nbre = 0;
+            String saisie;
+            do
+            {
+                Console.Write("Entrez un nombre : ");
+                saisie = Console.ReadLine();
+                nbre = Convert.ToInt32(saisie);
+                unCarton.Marquer(nbre);
+                pres.Afficher(unCarton);
+            } while (nbre != 0);
+            Console.ReadLine();
         }
     }
 }
