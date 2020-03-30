6bf04b440d004c11f67a6f17a872d0edd72c1e69
Fabrice Foray <farbice.foray@lyceeastier.com>
2020-03-30 13:36:26 +0200

    (Carton) Ajout de Verifier, Marquer, EstQuine, EstPlein

--- JeuDuLoto_Console/JeuDuLoto_Console/Carton.cs
+++ JeuDuLoto_Console/JeuDuLoto_Console/Carton.cs
@@ -27,25 +27,25 @@ namespace JeuDuLoto_Console
             lignes = new Ligne[3];
         }
 
-        public Carton(int[,] contenu):this()
+        public Carton(int[,] contenu) : this()
         {
             if (contenu == null)
                 throw new NullReferenceException("Le tableau doit exister");
             // Normalement, on a 3 x 5 numeros
-            if (contenu.Length != 15 )
+            if (contenu.Length != 15)
                 throw new Exception("Le tableau doit avoir une taille de 3 x 5");
             if (contenu.Rank != 2)
                 throw new Exception("Le tableau doit avoir deux dimensions : 3 x 5");
-            if (contenu.GetUpperBound(0) != 2 ) // Le plus grand Index dans la Dimension 0, c'est 2
+            if (contenu.GetUpperBound(0) != 2) // Le plus grand Index dans la Dimension 0, c'est 2
                 throw new Exception("Le tableau doit avoir deux dimensions, la première de longueur 3");
             if (contenu.GetUpperBound(1) != 4)  // Le plus grand Index dans la Dimension 1, c'est 4
                 throw new Exception("Le tableau doit avoir deux dimensions, la deuxième de longueur 5");
             // Pour chaque Ligne
-            for ( int i =0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 // On copie dans un tableau
                 int[] temp = new int[5];
-                for( int j = 0; j<5;j++)
+                for (int j = 0; j < 5; j++)
                 {
                     temp[j] = contenu[i, j];
                 }
@@ -71,6 +71,68 @@ namespace JeuDuLoto_Console
                 }
                 // Non => Exception
                 throw new IndexOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Vérifie si le nombre existe dans le carton
+        /// </summary>
+        /// <param name="nombreAVerifier"></param>
+        /// <returns></returns>
+        public bool Verifier(int nombreAVerifier)
+        {
+            bool exist = false;
+            for (int i = 0; i < lignes.Length; i++)
+            {
+                exist = exist || this.lignes[i].Verifier(nombreAVerifier);
+            }
+            return exist;
+        }
+
+        /// <summary>
+        /// Essaye de marquer un nombre dans le carton
+        /// </summary>
+        /// <param name="NombreAMarquer"></param>
+        /// <returns></returns>
+        public bool Marquer(int NombreAMarquer)
+        {
+            bool exist = false;
+            for (int i = 0; i < lignes.Length; i++)
+            {
+                exist = exist || this.lignes[i].Marquer(NombreAMarquer);
+            }
+            return exist;
+        }
+
+        /// <summary>
+        /// Indique si le carton contient au moins une Quine
+        /// </summary>
+        public bool EstQuine
+        {
+            get
+            {
+                bool quine = false;
+                for (int i = 0; i < lignes.Length; i++)
+                {
+                    quine = quine || this.lignes[i].EstQuine;
+                }
+                return quine;
+            }
+        }
+
+        /// <summary>
+        /// Indique si le carton est plein == Toutes les lignes sont des Quine
+        /// </summary>
+        public bool EstPlein
+        {
+            get
+            {
+                bool plein = false;
+                for (int i = 0; i < lignes.Length; i++)
+                {
+                    plein = plein && this.lignes[i].EstQuine;
+                }
+                return plein;
             }
         }
     }
